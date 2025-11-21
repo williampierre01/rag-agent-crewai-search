14529ba7b2cd61d1726fdd84345dcd9762372b8c
williampierre01 <74018373+williampierre01@users.noreply.github.com>
2025-11-21 06:43:44 +0000

    main update

---
+++ src_agentic_rag/main.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+import sys
+import warnings
+
+from agentic_rag.crew import AgenticRag
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'query': 'Who is elon musk?'
+    }
+    AgenticRag().crew().kickoff(inputs=inputs)
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        "topic": "AI LLMs"
+    }
+    try:
+        AgenticRag().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        AgenticRag().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {
+        "topic": "AI LLMs"
+    }
+    try:
+        AgenticRag().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
